7ea8b2ec4b341a7f1dee064b8aa6cc2b5e70f62e
DarkDeadly <omrianis2@gmail.com>
2025-05-20 02:56:58 +0200

    Call to Action Section + responsive done

---
+++ src/Component/homepageComponents/Cta.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import "./cta.css"
+import "../../util/global.css"
+const Cta = () => {
+  return (
+    <div className='Cta__Content'>
+        <h3>Take Full Control of Your Inventory</h3>
+        <p>Say goodbye to stockouts and overstocking. With StockMaster, you get real-time 
+            visibility, smart tracking, and effortless inventory management — all in one 
+            platform. It's time to level up your business operations.</p>
+        <button className='navBtn'>Explore Features</button>
+    </div>
+  )
+}
+
+export default Cta
--- src/Pages/HomePage/HomePage.jsx
+++ src/Pages/HomePage/HomePage.jsx
@@ -3,11 +3,12 @@ import '../../util/global.css'
 import './homepage.css'
 import Btn from '../../Component/button/Btn'
 import HomePageCarousel from '../../Component/homepageComponents/HomePageCarousel'
-import {imageDataCarousel , Features} from "../../util/util.js"
+import { imageDataCarousel, Features } from "../../util/util.js"
 import { Carousel } from 'antd'
 import FeatureImage from "../../assets/FeatureImage.png"
 import HomePageFeatures from '../../Component/homepageComponents/HomePageFeatures'
 import { useState } from 'react'
+import Cta from '../../Component/homepageComponents/Cta.jsx'
 const HomePage = () => {
   const [Selected, setSelected] = useState(null)
   const FeatureSelected = (item) => {
@@ -16,44 +17,48 @@ const HomePage = () => {
 
   return (
     <>
-    <Header/>
-    <section className="Hero__Section">
-      <h2>Modern Stock Management System</h2>
-      <p>Streamline your inventory with our cutting-edge stock management system. Take control of your stock 
-      levels, orders and more with ease.
-      </p>
-      <div className="Hero__sectionBtns">
-        <Btn btnText='Get Started' btnClass='navBtn'/>
-        <Btn btnText='Learn More'  btnClass='navBtn Hero__btn'/>
-      </div>
-    </section>
-            <Carousel autoplay  className='Carousel__Wrapper' autoplaySpeed={3000}> 
-            {imageDataCarousel.map((item) => (
-                <HomePageCarousel 
-                  CarouselImage={item.CarouselImage} 
-                  CarouselHeading={item.CarouselHeading} 
-                  CarousselText={item.CarouselText} 
-            />
-            ))}
-          </Carousel>     
-    <section className="feature__Section">
-      <h2>StockMaster Features</h2>
-    <div className="features__Section">
-      <img src={FeatureImage} alt="featureImg" />
-       <div className="Feature__content">
-            {Features.map((item) => (
-              <HomePageFeatures 
-              key={item.id}
-              Feature__Text={item.FeatureText} 
-              Feature__Title={item.FeatureHeadLine}
-              FeatureClass={`FeatureContainer ${Selected === item.id ? "Active" : ""}`}
-              OnClickFn={() => FeatureSelected(item.id)}
+      <Header />
+      <section className="Hero__Section">
+        <h2>Modern Stock Management System</h2>
+        <p>Streamline your inventory with our cutting-edge stock management system. Take control of your stock
+          levels, orders and more with ease.
+        </p>
+        <div className="Hero__sectionBtns">
+          <Btn btnText='Get Started' btnClass='navBtn' />
+          <Btn btnText='Learn More' btnClass='navBtn Hero__btn' />
+        </div>
+      </section>
+      <Carousel autoplay className='Carousel__Wrapper' autoplaySpeed={3000}>
+        {imageDataCarousel.map((item) => (
+          <HomePageCarousel
+            CarouselImage={item.CarouselImage}
+            CarouselHeading={item.CarouselHeading}
+            CarousselText={item.CarouselText}
+          />
+        ))}
+      </Carousel>
+      <section className="feature__Section">
+        <h2>StockMaster Features</h2>
+        <div className="features__Section">
+          <img src={FeatureImage} alt="featureImg" />
+          <div className="Feature__content">
+            {
+            Features.map((item) => (
+              <HomePageFeatures
+                key={item.id}
+                Feature__Text={item.FeatureText}
+                Feature__Title={item.FeatureHeadLine}
+                FeatureClass={`FeatureContainer ${Selected === item.id ? "Active" : ""}`}
+                OnClickFn={() => FeatureSelected(item.id)}
               />
 
             ))}
-        </div>   
-    </div>    
-    </section>
+          </div>
+        </div>
+      </section>
+      <section className="cta__section">
+        <Cta/>
+      </section>
     </>
   )
 }
