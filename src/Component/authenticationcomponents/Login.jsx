ecc28ca233bfe978ff64094bd99699fef52f1687
DarkDeadly <omrianis2@gmail.com>
2025-05-24 03:12:00 +0200

    login form validation ui

--- src/Component/authenticationcomponents/Login.jsx
+++ src/Component/authenticationcomponents/Login.jsx
@@ -3,12 +3,21 @@ import { Input } from 'antd'
 import React, { useState } from 'react'
 import './login.css'
 import { useNavigate } from 'react-router'
+import { EmailVerif } from '../../util/util.js'
+
 const Login = () => {
     const [passHidden, setpassHidden] = useState(false)
+    const [Email, setEmail] = useState("")
     const navigate = useNavigate()
+   
+    const inputClass = () => {
+        if (!Email) return '';
+        return EmailVerif(Email) ? "valid" : "invalid"
+    }
     const ShowPass = () => {
         setpassHidden(!passHidden)
     }
+   
     return (
         <>
             <section className="login__Container">
@@ -16,12 +25,22 @@ const Login = () => {
                     <div className="form__Box Login">
                         <h1>Welcome Back to StockMaster</h1>
                         <form action="">
-                            <Input size="large" placeholder="you@example.com" prefix={<MailOutlined />} className='Input__Box' />
-                            <Input size="large" placeholder="Enter your Password" type={passHidden ? 'text' : 'password'} prefix={<LockOutlined />} className='Input__Box'  suffix = {passHidden ? <EyeInvisibleOutlined onClick={ShowPass} /> : <EyeOutlined onClick={ShowPass}/>}/>
+                            <Input
+                                size="large"
+                                placeholder="you@example.com"
+                                type='email'
+                                prefix={<MailOutlined />}
+                                className={`Input__Box ${inputClass()}`}
+                                onChange={(e) => setEmail(e.target.value)}
+                            />
+                            {Email && EmailVerif(Email) !== true && (
+                                <p className="error-message">{EmailVerif(Email)}</p>
+                            )}                            
+                            <Input size="large" placeholder="Enter your Password" type={passHidden ? 'text' : 'password'} prefix={<LockOutlined />} className='Input__Box' suffix={passHidden ? <EyeInvisibleOutlined onClick={ShowPass} /> : <EyeOutlined onClick={ShowPass} />} />
                             <span className='forget__pass'>Forget Password</span>
                             <div className="login__button">
-                            <button className='navBtn LoginBtn'>Login</button>
-                            <button className='navBtn LoginBtn register' onClick={()=> navigate('/Authentication/Register')}>Register</button>
+                                <button className='navBtn LoginBtn' >Login</button>
+                                <button className='navBtn LoginBtn register' onClick={() => navigate('/Authentication/Register')}>Register</button>
 
                             </div>
                             <p>or sign in with social Media</p>
@@ -34,9 +53,9 @@ const Login = () => {
                     </div>
                     <div className="register__Annoucement">
                         <div className="register__content">
-                        <h1>Hello , Welcome to StockMaster</h1>
-                        <p>don't have an account ?</p>
-                        <button className='navBtn RegisterBtn' onClick={()=> navigate('/Authentication/Register')}>Register</button>
+                            <h1>Hello , Welcome to StockMaster</h1>
+                            <p>don't have an account ?</p>
+                            <button className='navBtn RegisterBtn' onClick={() => navigate('/Authentication/Register')}>Register</button>
                         </div>
                     </div>
                 </div>
