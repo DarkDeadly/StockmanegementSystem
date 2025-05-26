7082ab455faca1fa1f0c7010c0423f4fb889be64
DarkDeadly <omrianis2@gmail.com>
2025-05-26 20:30:03 +0200

    UserManagement Done + responsive

---
+++ src/Component/UserManagementComponent/UserTable.jsx
@@ -0,0 +1,74 @@
+import { collection, doc, getDoc, getDocs, onSnapshot, updateDoc } from 'firebase/firestore'
+import React, { useEffect, useState } from 'react'
+import { db } from '../../util/firebase'
+import { Button, Table } from 'antd';
+
+const UserTable = () => {
+    const [users, setUsers] = useState([]);
+
+    useEffect(() => {
+        const unsubscribe = onSnapshot(collection(db, "Users"), (snapshot) => {
+            setUsers(snapshot.docs.map((doc) => ({
+                id: doc.id,
+                key: doc.id,
+                ...doc.data(),
+            })));
+        });
+
+        return () => unsubscribe(); 
+    }, []);
+
+const handlePromoteAdmin = async (record) => {
+  console.log('User Data:', record); // ✅ This logs the table row
+  const docRef = doc(db, "Users", record.id);
+  try {
+    await updateDoc(docRef , {role : "admin"})
+  } catch (error) {
+    console.log(error)
+  }
+};
+const handleDemote = async (record) => {
+  console.log('User Data:', record); // ✅ This logs the table row
+  const docRef = doc(db, "Users", record.id);
+  try {
+    await updateDoc(docRef , {role : "user"})
+  } catch (error) {
+    console.log(error)
+  }
+};
+
+const columns = [
+  { title: 'username', dataIndex: 'username', key: 'username' },
+  { title: 'email', dataIndex: 'email', key: 'email' , responsive: ['sm']},
+  { title: 'role', dataIndex: 'role', key: 'role' },
+  {
+    title: 'Action',
+    dataIndex: '',
+    key: 'x',
+    render: (_, record) => (
+        <div>
+        <Button type="link" onClick={() => handlePromoteAdmin(record)}>
+          Promote Admin
+        </Button>
+        <Button type="link" onClick={() => handleDemote(record)}>
+          demote
+        </Button>
+        </div>
+    )
+  },
+];
+
+
+  return (
+    <div>
+     <Table
+    columns={columns}
+  
+    dataSource={users}
+    
+  />
+    </div>
+  )
+}
+
+export default UserTable
--- src/Pages/UserManagement/UserManagement.jsx
+++ src/Pages/UserManagement/UserManagement.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Header from '../../Component/header/Header'
+import UserTable from '../../Component/UserManagementComponent/UserTable'
 
 const UserManagement = () => {
   return (
     <div>
         <Header/>
+        <UserTable/>
     </div>
   )
 }
